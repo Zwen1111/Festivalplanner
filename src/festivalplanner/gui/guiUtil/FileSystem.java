7939ba2739136ad138d6decf0480f8eac038f0bb
Maartenwn <maartenwn@gmail.com>
2017-02-12 23:45:50 +0100

    added a starting name when saving or opening

--- src/festivalplanner/gui/guiUtil/FileSystem.java
+++ src/festivalplanner/gui/guiUtil/FileSystem.java
@@ -14,6 +14,7 @@ import java.util.Collection;
 public class FileSystem {
     private File file;
     private Database database;
+    private Database databaseOld;
 
     public FileSystem(Database database) {
         this.database = database;
@@ -37,6 +38,7 @@ public class FileSystem {
         } else {
             try (ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(this.file))) {
                 output.writeObject(database.getPerformances());
+                databaseOld = database;
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -44,9 +46,12 @@ public class FileSystem {
     }
 
     public void saveAs() {
-        JFileChooser fc = new JFileChooser();
-        fc.setFileFilter(new FileNameExtensionFilter("database of the agenda ", "fd"));
 
+        JFileChooser fc = new JFileChooser();
+        if(file == null) {
+            fc.setSelectedFile(new File("Festival_planner_1.fd"));
+        }else fc.setSelectedFile(file);
+        fc.setFileFilter(new FileNameExtensionFilter("database of the agenda ", "fd"));
 
         int returnVal = fc.showSaveDialog(null);
         if (returnVal == JFileChooser.APPROVE_OPTION) {
@@ -57,18 +62,21 @@ public class FileSystem {
                 betterFile = new File(file.getAbsolutePath() + ".fd");
             } else {
 
-                int confirmcode = JOptionPane.showConfirmDialog(null, "File alreadyexist. Do you want to overwrite the file");
-                if (confirmcode == JOptionPane.CANCEL_OPTION) {
-                    saveAs();
-                    return;
-                } else if (confirmcode == JOptionPane.NO_OPTION) {
-                    return;
-                }
-                betterFile = new File(file.getAbsolutePath());
+                if(file.exists()) {
+                    int confirmcode = JOptionPane.showConfirmDialog(null, "File already exists. Do you want to overwrite the file");
+                    if (confirmcode == JOptionPane.CANCEL_OPTION) {
+                        saveAs();
+                        return;
+                    } else if (confirmcode == JOptionPane.NO_OPTION) {
+                        return;
+                    }
+
+                }betterFile = new File(file.getAbsolutePath());
 
             }
             try (ObjectOutputStream output = new ObjectOutputStream(new FileOutputStream(betterFile))) {
                 output.writeObject(database.getPerformances());
+                databaseOld = database;
             } catch (Exception e) {
                 e.printStackTrace();
             }
@@ -77,12 +85,17 @@ public class FileSystem {
     }
 
     public void open() {
+
         if(askForSaving() == false)
         {
             return;
         }else {
 
             JFileChooser fc = new JFileChooser();
+            if(file == null) {
+                fc.setSelectedFile(new File("Festival_planner_1.fd"));
+            }else fc.setSelectedFile(file);
+
             fc.setFileFilter(new FileNameExtensionFilter("Database of the agenda .fd ", "fd"));
 
             int returnVal = fc.showOpenDialog(null);
@@ -107,13 +120,17 @@ public class FileSystem {
 
     private boolean askForSaving()
     {
-        int confirmCode = JOptionPane.showConfirmDialog(null, "Do want to save changes");
-        if (confirmCode == JOptionPane.OK_OPTION) {
-            saveAs();
+        if(file == null || database.equals(databaseOld)) {
+            int confirmCode = JOptionPane.showConfirmDialog(null, "Do want to save changes");
+            if (confirmCode == JOptionPane.OK_OPTION) {
+                saveAs();
 
-        }else if (confirmCode == JOptionPane.CANCEL_OPTION || confirmCode == JOptionPane.CLOSED_OPTION) {
-            return false;
-        }
-        return true;
+            } else if (confirmCode == JOptionPane.CANCEL_OPTION || confirmCode == JOptionPane.CLOSED_OPTION) {
+                return false;
+            }
+
+        }return true;
     }
+
+
 }

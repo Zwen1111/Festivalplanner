27cea037dff7724f9b2a2a1e6821e963dfde00a4
Lars Moesman <lmoesman@student.avans.nl>
2017-02-13 16:15:57 +0100

    Fuck sort mechanism!!

--- src/festivalplanner/gui/table/CalendarTable.java
+++ src/festivalplanner/gui/table/CalendarTable.java
@@ -8,13 +8,17 @@ import festivalplanner.gui.AddPerformanceButton;
 
 import javax.swing.*;
 import javax.swing.table.AbstractTableModel;
+import javax.swing.table.TableModel;
+import javax.swing.table.TableRowSorter;
 import java.awt.*;
 import java.awt.event.ActionEvent;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
 import java.time.LocalTime;
 import java.util.*;
 import java.util.List;
 
-public class CalendarTable extends JPanel implements Database.OnDataChangedListener{
+public class CalendarTable extends JPanel implements Database.OnDataChangedListener {
 
     private AbstractTableModel model;
     private ArrayList<Performance> performancesSorted;
@@ -26,66 +30,66 @@ public class CalendarTable extends JPanel implements Database.OnDataChangedListe
 
     public CalendarTable(Database database) {
         JTable table = new JTable();
-         stageComboBox = new JComboBox();
-		AddPerformanceButton addButton = new AddPerformanceButton(database);
+        stageComboBox = new JComboBox();
+        AddPerformanceButton addButton = new AddPerformanceButton(database);
         setName("Table");
-		this.database = database;
-		index = 0;
-		comboBoxItems = new ArrayList<>();
+        this.database = database;
+        index = 0;
+        comboBoxItems = new ArrayList<>();
 
         filterPerformances();
 
-        LocalTime timeTable = LocalTime.of(0,0);
-        table.setModel(model = new AbstractTableModel()
-        {
+        LocalTime timeTable = LocalTime.of(0, 0);
+        table.setModel(model = new AbstractTableModel() {
             @Override
-            public int getRowCount () {
+            public int getRowCount() {
 
-            return 48;
-        }
-
-            @Override
-            public int getColumnCount () {
-            return 4;
-        }
-
-            @Override
-            public Object getValueAt ( int rowIndex, int columnIndex){
-            switch (columnIndex) {
-                case 0:
-                    return timeTable.plusMinutes(30 * rowIndex).toString();
-                case 1:
-                    if ((performanceTimeCheck(timeTable.plusMinutes(30 * rowIndex)) != null)) {
-                    return (performanceTimeCheck(timeTable.plusMinutes(30 * rowIndex))).getArtistNames();
-                }
-
-                case 2:
-                    if ((performanceTimeCheck(timeTable.plusMinutes(30 * rowIndex)) != null)) {
-                        return (performanceTimeCheck(timeTable.plusMinutes(30 * rowIndex))).generatePopularity();
-                    }
-
-                case 3:
-                    if ((performanceTimeCheck(timeTable.plusMinutes(30 * rowIndex)) != null)) {
-                        return (performanceTimeCheck(timeTable.plusMinutes(30 * rowIndex))).getArtistGenres();
-                    }
-                default:
-                    return null;
-
-
+                return 48;
             }
-        }
+
             @Override
-            public String getColumnName ( int column){
-            if (column == 0)
-                return "Time";
-            else if (column == 1)
-                return "Artist";
-            else if (column == 2)
-                return "Popularity";
-            else if (column == 3)
-                return "Genre";
-            return "";
-        }
+            public int getColumnCount() {
+                return 4;
+            }
+
+            @Override
+            public Object getValueAt(int rowIndex, int columnIndex) {
+                switch (columnIndex) {
+                    case 0:
+                        return timeTable.plusMinutes(30 * rowIndex).toString();
+                    case 1:
+                        if ((performanceTimeCheck(timeTable.plusMinutes(30 * rowIndex)) != null)) {
+                            return (performanceTimeCheck(timeTable.plusMinutes(30 * rowIndex))).getArtistNames();
+                        }
+
+                    case 2:
+                        if ((performanceTimeCheck(timeTable.plusMinutes(30 * rowIndex)) != null)) {
+                            return (performanceTimeCheck(timeTable.plusMinutes(30 * rowIndex))).generatePopularity();
+                        }
+
+                    case 3:
+                        if ((performanceTimeCheck(timeTable.plusMinutes(30 * rowIndex)) != null)) {
+                            return (performanceTimeCheck(timeTable.plusMinutes(30 * rowIndex))).getArtistGenres();
+                        }
+                    default:
+                        return null;
+
+
+                }
+            }
+
+            @Override
+            public String getColumnName(int column) {
+                if (column == 0)
+                    return "Time";
+                else if (column == 1)
+                    return "Artist";
+                else if (column == 2)
+                    return "Popularity";
+                else if (column == 3)
+                    return "Genre";
+                return "";
+            }
 
 
         });
@@ -97,6 +101,16 @@ public class CalendarTable extends JPanel implements Database.OnDataChangedListe
             filterPerformances();
             this.repaint();
         });
+
+//        TableRowSorter<TableModel> sorter = new TableRowSorter<>(model);
+//
+//
+//        List<RowSorter.SortKey> sortKeys = new ArrayList<>();
+//        sortKeys.add(new RowSorter.SortKey(1, SortOrder.ASCENDING));
+//        sortKeys.add(new RowSorter.SortKey(3, SortOrder.ASCENDING));
+//        sorter.setSortKeys(sortKeys);
+//        table.setRowSorter(sorter);
+
 
         index = 0;
         mainScreen = new JPanel(new BorderLayout());
@@ -123,8 +137,8 @@ public class CalendarTable extends JPanel implements Database.OnDataChangedListe
         this.add(mainScreen);
     }
 
-    public void fillComboBox(){
-        for (Stage stage:database.getStages()) {
+    public void fillComboBox() {
+        for (Stage stage : database.getStages()) {
             if (!comboBoxItems.contains(stage.getName())) {
                 comboBoxItems.add(stage.getName());
                 stageComboBox.addItem(stage.getName());
@@ -142,14 +156,16 @@ public class CalendarTable extends JPanel implements Database.OnDataChangedListe
     }
 
     public void filterPerformances() {
-    	List<Stage> stages = database.getStages();
-    	if (stages.size() > 0) {filterByStage(stages.get(index));}
+        List<Stage> stages = database.getStages();
+        if (stages.size() > 0) {
+            filterByStage(stages.get(index));
+        }
 
     }
 
     public Performance performanceTimeCheck(LocalTime timeTable) {
         try {
-            for(Performance p : performancesSorted) {
+            for (Performance p : performancesSorted) {
                 if (timeTable.compareTo(p.getStartTime()) >= 0 && timeTable.compareTo(p.getEndTime()) < 0) {
                     return p;
                 }
@@ -163,20 +179,20 @@ public class CalendarTable extends JPanel implements Database.OnDataChangedListe
     @Override
     public void onDataChanged() {
 
-     if(database.getPerformances().size() == 0) {
-         index = 0;
-         performancesSorted = new ArrayList<>();
-     }
-     if(database.getStages().size() == 0)
-     {
-      stageComboBox.removeAllItems();
-      comboBoxItems.clear();
-     }else {
-         fillComboBox();
-     }
+        if (database.getPerformances().size() == 0) {
+            index = 0;
+            performancesSorted = new ArrayList<>();
+        }
+        if (database.getStages().size() == 0) {
+            stageComboBox.removeAllItems();
+            comboBoxItems.clear();
+        } else {
+            fillComboBox();
+        }
 
-     model.fireTableDataChanged();
-     repaint();
+        model.fireTableDataChanged();
+        repaint();
 
     }
 }
+

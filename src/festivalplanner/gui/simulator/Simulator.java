76b462246121841e2cb077ad0b88645d4393e020
Maartenwn <maartenwn@gmail.com>
2017-02-20 13:01:10 +0100

    added tilesets

--- src/festivalplanner/gui/simulator/Simulator.java
+++ src/festivalplanner/gui/simulator/Simulator.java
@@ -1,15 +1,57 @@
 package festivalplanner.gui.simulator;
 
-import javax.imageio.ImageIO;
+import javax.json.Json;
+import javax.json.JsonObject;
+import javax.json.JsonReader;
 import javax.swing.*;
 import java.awt.*;
-import java.awt.geom.Rectangle2D;
-import java.awt.image.BufferedImage;
+import java.awt.geom.AffineTransform;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.io.InputStream;
 
 /**
  * @author Maarten Nieuwenhuize
  */
 public class Simulator extends JPanel {
+    TilesetManager t;
+
+    public static void main(String[] args) {
+
+        JsonObject jsonObject = null;
+
+        try (
+                InputStream is = new FileInputStream("resources\\maps\\Map+Colliosion.json");
+                JsonReader js = Json.createReader(is);
+        ) {
+
+            jsonObject = js.readObject();
+
+        }catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        Simulator simulator = new Simulator();
+        if(jsonObject == null) {
+            System.err.println("jsonObject == null");
+        }else {
+
+
+            simulator.t = new TilesetManager(jsonObject.getJsonArray("tilesets"));
+
+
+        }
+
+        JFrame frame = new JFrame();
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        frame.setSize(1000,1000);
+        frame.setVisible(true);
+        frame.setContentPane(simulator);
+
+    }
 
 
     public Simulator() {
@@ -23,18 +65,6 @@ public class Simulator extends JPanel {
         Graphics2D g2d = (Graphics2D) g;
 
 
-        Rectangle2D.Double background = new Rectangle2D.Double(0,0,2000,960);
-        BufferedImage texture;
-        try
-        {
-            texture = ImageIO.read(getClass().getResource("/backgroundTest.png"));
-            g2d.setPaint(new TexturePaint(texture, new Rectangle2D.Double(0,0,1920,960)));
-        }
-        catch (Exception e)
-        {
-            e.printStackTrace();
-        }
-
-        g2d.fill(background);
+        g2d.drawImage(t.getImage(5),new AffineTransform(),null);
     }
 }

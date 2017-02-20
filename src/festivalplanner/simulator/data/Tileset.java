33c48c03f86b1ca5f8b68eb108f344bac37e5353
Maartenwn <maartenwn@gmail.com>
2017-02-20 12:50:10 +0100

    Made the tilesetManager and the tileset classes

---
+++ src/festivalplanner/simulator/data/Tileset.java
@@ -0,0 +1,65 @@
+package festivalplanner.simulator.data;
+
+import javax.imageio.ImageIO;
+import javax.json.JsonObject;
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by Maarten on 20/02/2017.
+ */
+public class Tileset {
+     private int collums;
+     private int firstgid;
+     private BufferedImage image;
+
+     private int imageHeight;
+     private int imageWidth;
+     private int margin;
+     private String name;
+     private int spacing;
+     private int tileCount;
+     private int tileHeight;
+     private int tileWidth;
+
+    /**
+     * makes an tileset
+     *
+     * @param tileset an array of tilesets;
+     *
+     */
+    public Tileset(JsonObject tileset) {
+         try {
+              collums= tileset.getInt("columns");
+              firstgid = tileset.getInt("firstgid");
+              String imageName = tileset.getString("image");
+              image = ImageIO.read(getClass().getResourceAsStream("/tilesets/" + imageName));
+              imageHeight = tileset.getInt("imageheight");
+              imageWidth = tileset.getInt("imagewidth");
+              margin = tileset.getInt("margin");
+              name = tileset.getString("name");
+              spacing = tileset.getInt("spacing");
+              tileCount = tileset.getInt("tilecount");
+              tileHeight = tileset.getInt("tileheight");
+              tileWidth = tileset.getInt("tilewidth");
+         }catch (Exception e) {
+              System.err.println("Tileset " + name + " could not be loaded");
+              e.printStackTrace();
+         }
+    }
+     public int getCollums() {return collums;}
+     //public int getFirstgid() {return firstgid;}
+     public BufferedImage getImage() {
+          return image;
+     }
+     public int getImageHeight() {
+          return imageHeight;
+     }
+     //public int getImageWidth() {return imageWidth;}
+     //public int getTileCount() {return tileCount;}
+     public int getTileHeight() {
+          return tileHeight;
+     }
+     public int getTileWidth() {
+          return tileWidth;
+     }
+}
